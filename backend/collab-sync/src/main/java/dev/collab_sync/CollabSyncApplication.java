8a1a5a00781d4ccb93b0a086d9e347cd714f931f
J.Seo <67002666+JSeo227@users.noreply.github.com>
2025-08-07 09:54:02 +0900

    backend

---
+++ backend/collab-sync/src/main/java/dev/collab_sync/CollabSyncApplication.java
@@ -0,0 +1,13 @@
+package dev.collab_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabSyncApplication.class, args);
+	}
+
+}
---
+++ backend/collab-sync/src/test/java/dev/collab_sync/CollabSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.collab_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
