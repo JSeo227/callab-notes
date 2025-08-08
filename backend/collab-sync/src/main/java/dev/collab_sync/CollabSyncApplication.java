e8b7daca4001a1f8be50653118fdd3210c49a6a1
J.Seo <67002666+JSeo227@users.noreply.github.com>
2025-08-08 11:08:45 +0900

    Update CollabSyncApplication.java

--- backend/collab-sync/src/main/java/dev/collab_sync/CollabSyncApplication.java
+++ backend/collab-sync/src/main/java/dev/collab_sync/CollabSyncApplication.java
@@ -1,13 +1,17 @@
 package dev.collab_sync;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class CollabSyncApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollabSyncApplication.class, args);
-	}
+        log.info("Spring Boot Application started");
+
+    }
 
 }
