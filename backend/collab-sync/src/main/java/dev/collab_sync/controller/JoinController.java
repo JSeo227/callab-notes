69c22a1666cb217a94cdf972b81962a26fdcb4f1
J.Seo <67002666+JSeo227@users.noreply.github.com>
2025-08-11 13:18:51 +0900

    Update JoinController.java

--- backend/collab-sync/src/main/java/dev/collab_sync/controller/JoinController.java
+++ backend/collab-sync/src/main/java/dev/collab_sync/controller/JoinController.java
@@ -4,7 +4,6 @@ import dev.collab_sync.controller.dto.JoinDto;
 import dev.collab_sync.service.JoinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +13,7 @@ public class JoinController {
     private final JoinService joinService;
 
     @PostMapping("/join")
-    public String join(@RequestBody JoinDto joinDto) {
+    public String join(JoinDto joinDto) {
         joinService.join(joinDto);
         return "success";
     }
