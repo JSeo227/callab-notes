3abdc790bcecc074d286b66de0810c2330612110
J.Seo <67002666+JSeo227@users.noreply.github.com>
2025-08-12 14:55:57 +0900

    스케줄러 추가

---
+++ backend/collab-sync/src/main/java/dev/collab_sync/RefreshTokenSchedular.java
@@ -0,0 +1,21 @@
+package dev.collab_sync;
+
+import dev.collab_sync.domain.refresh.Refresh;
+import dev.collab_sync.domain.refresh.RefreshService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class RefreshTokenSchedular {
+
+    private final RefreshService refreshService;
+
+    @Scheduled(fixedRate = 60000)
+    public void deleteByRefreshToken(LocalDateTime expired) {
+        refreshService.deleteByExpired(expired);
+    }
+}
